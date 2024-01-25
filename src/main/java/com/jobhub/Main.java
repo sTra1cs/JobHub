cfae74f76d3bc209b29e5b22e66d89efd9cec264
Anatoly Brezgun <brezgun001@gmail.com>
2024-01-25 21:26:27 +0300

    add to class job and jobservice

--- src/main/java/com/jobhub/Main.java
+++ src/main/java/com/jobhub/Main.java
@@ -2,6 +2,5 @@ package com.jobhub;
 
 public class Main {
     public static void main(String[] args) {
-
     }
 }
--- src/main/java/com/jobhub/service/JobService.java
+++ src/main/java/com/jobhub/service/JobService.java
@@ -1,4 +1,15 @@
 package com.jobhub.service;
 
+import com.jobhub.model.Job;
+
+import java.util.List;
+
 public class JobService {
+    public List <Job> getAllJobs(){
+        return null;
+    }
+
+    public Job createJob(Job job){
+        return null;
+    }
 }
